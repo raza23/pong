16a56cf92e1eeb93f920687b29c5cad5cc796009
Raza <rtshareef@gmail.com>
2020-05-14 07:59:56 -0400

    refactoring w/ classes

---
+++ pong0/Ball.lua
@@ -0,0 +1,25 @@
+Ball = Class{}
+
+function Ball:init(x,y,width,height)
+    self.x = x 
+    self.y = y 
+    self.width = width
+    self.height = height
+
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50,50) 
+end
+
+
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+
+end
+
+
+function Ball:render()
+    love.graphics.rectangle('fill',self.x,self.y,5,5)
+
+end
--- pong0/main.lua
+++ pong0/main.lua
@@ -8,8 +8,11 @@ PADDLE_SPEED = 200
 -- Give the window a better look, more retro.
 -- game runs on the virtual coordinates but window size stays the same
 -- game is almost like zoomed in
-
+Class = require 'class'
 push = require 'push'
+
+require 'Ball'
+require 'Paddle'
 
 function love.load()
     math.randomseed(os.time())
@@ -22,15 +25,19 @@ function love.load()
     player2score = 3
     
     -- paddle positions
-    player1y = 30
-    player2y = VIRTUAL_HEIGHT - 40
+    paddle1 = Paddle(5,20,5,20)
+    paddle2 = Paddle(VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT - 30,5,20)
 
+    -- *player1y = 30
+    -- *player2y = VIRTUAL_HEIGHT - 40
+    
     -- ball positinos
     ballx = VIRTUAL_WIDTH/2-2
     bally = VIRTUAL_HEIGHT/2-2
+    ball = Ball(ballx,bally,5,5)
     -- like a ternary
-    balldx = math.random(2) == 1 and -100 or 100
-    balldy = math.random(-50,50) 
+    -- *balldx = math.random(2) == 1 and -100 or 100
+    -- *balldy = math.random(-50,50) 
 
     gameState = 'start'
     
@@ -50,36 +57,44 @@ function love.load()
 end
 
 function love.update(dt)
+    paddle1:update(dt)
+    paddle2:update(dt)
+
     if love.keyboard.isDown('w') then
-        player1y = math.max(0,player1y - PADDLE_SPEED * dt)
-
+        -- player1y = math.max(0,player1y - PADDLE_SPEED * dt)
+        paddle1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1y = math.min(VIRTUAL_HEIGHT - 20,player1y + PADDLE_SPEED * dt)
-
+        -- player1y = math.min(VIRTUAL_HEIGHT - 20,player1y + PADDLE_SPEED * dt)
+        paddle1.dy = PADDLE_SPEED
+    else
+        paddle1.dy = 0
     end
 
     if love.keyboard.isDown('up') then
-        player2y = math.max(0,player2y - PADDLE_SPEED * dt)
-
+        -- * player2y = math.max(0,player2y - PADDLE_SPEED * dt)
+        paddle2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2y = math.min(VIRTUAL_HEIGHT - 20,player2y + PADDLE_SPEED * dt)
-
+        -- * player2y = math.min(VIRTUAL_HEIGHT - 20,player2y + PADDLE_SPEED * dt)
+        paddle2.dy = PADDLE_SPEED
+    else    
+        paddle2.dy = 0
     end
 
     if gameState == 'play' then
-        ballx = ballx + balldx * dt
-        bally = bally + balldy * dt
+        ball:update(dt)
+        -- *ballx = ballx + balldx * dt
+        -- *bally = bally + balldy * dt
     end
 
 
 end
 
 function gameStart()
-    ballx = VIRTUAL_WIDTH/2-2
-    bally = VIRTUAL_HEIGHT/2-2
+    ball.x = VIRTUAL_WIDTH/2-2
+    ball.y = VIRTUAL_HEIGHT/2-2
     -- like a ternary
-    balldx = math.random(2) == 1 and -100 or 100
-    balldy = math.random(-50,50) 
+    ball.dx = math.random(2) == 1 and -100 or 100
+    ball.dy = math.random(-50,50) 
 end
 
 
@@ -108,10 +123,13 @@ function love.draw()
     love.graphics.clear(130 / 255,12 / 255,52/255,255/255)
     
 --  draws ball
-    love.graphics.rectangle('fill',ballx,bally,5,5)
+    -- * love.graphics.rectangle('fill',ballx,bally,5,5)
+    ball:render()
 -- draws rectangles
-    love.graphics.rectangle('fill',5,player1y,5,20)
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH- 10,player2y,5,20)
+paddle1:render()
+paddle2:render()
+    -- * love.graphics.rectangle('fill',5,player1y,5,20)
+    -- * love.graphics.rectangle('fill',VIRTUAL_WIDTH- 10,player2y,5,20)
 
 
 
